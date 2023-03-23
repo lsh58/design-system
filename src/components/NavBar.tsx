300b0d464d3f7e5e5f29e9fb6b8ceb2ae21229d4
이승현 <kevin_lee@gooroomee.com>
2023-03-23 11:26:40 +0900

    prettier 설정 수정 및 formatting

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,24 +1,26 @@
 import React from "react";
 import Link from "next/link";
 
+const pages: { title: string; link: string }[] = [
+  { title: "Foundations", link: "/foundations" },
+  { title: "Components", link: "/components" },
+  { title: "Templates", link: "/templates" },
+];
+
 const NavBar = () => {
   return (
     <div className="flex justify-between w-full px-8 py-6 border-b">
       <div className="flex gap-16">
         <p className="text-xl font-bold">LOGO</p>
-        <ul className="flex gap-4">
-          <li>
-            <Link href="/">OVERVIEW</Link>
-          </li>
-          <li>
-            <Link href="/foundations">FOUNDATIONS</Link>
-          </li>
-          <li>
-            <Link href="/components">COMPONENTS</Link>
-          </li>
-          <li>
-            <Link href="/templates">TEMPLATES</Link>
-          </li>
+        <ul className="hidden gap-8 sm:flex">
+          {pages.map((page, index) => (
+            <li
+              key={index}
+              className="font-bold text-gray-400 transition duration-300 text-l hover:text-black"
+            >
+              <Link href={page.link}>{page.title}</Link>
+            </li>
+          ))}
         </ul>
       </div>
       <div>
